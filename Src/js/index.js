aee39e58fe41636cc9374859dbc34452dda0d0e7
Gur512 <gromana2022@gmail.com>
2025-07-06 16:26:11 -0500

    Added the toggle functonality

--- Src/js/index.js
+++ Src/js/index.js
@@ -29,3 +29,23 @@ listen('click', menuIcon, () => {
 listen('click', hideIcon, () => {
     sidebar.style.display = 'none';
 });
+
+/* Toggle logic */
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'light') {
+    body.classList.add('light-theme');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-theme');
+    
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
